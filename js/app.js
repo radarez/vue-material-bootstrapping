71c084c3ab49356e380c254c5d073ded0118a135
Adrian Miranda <miranda_adrian@hotmail.com>
2017-08-20 10:05:20 -0500

    First commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+Vue.use(VueMaterial);
+
+var App = new Vue({
+  el: '#app',
+   methods: {
+    /*Modal*/
+    openDialog(ref) {
+      this.$refs[ref].open();
+    },
+    closeDialog(ref) {
+      this.$refs[ref].close();
+    },
+    onOpen() {
+      console.log('Opened');
+    },
+    onClose(type) {
+      console.log('Closed', type);
+    },
+    /*Sidenav*/
+     toggleLeftSidenav() {
+      this.$refs.leftSidenav.toggle();
+    },
+    toggleRightSidenav() {
+      this.$refs.rightSidenav.toggle();
+    },
+    closeRightSidenav() {
+      this.$refs.rightSidenav.close();
+    },
+    open(ref) {
+      console.log('Opened: ' + ref);
+    },
+    close(ref) {
+      console.log('Closed: ' + ref);
+    }
+  }
+});
